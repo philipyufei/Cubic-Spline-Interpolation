1cc4ab69e7218f62d1e1c0f9e2338bcc2007732b
philipyufei <philipyufei@hotmail.com>
2016-12-30 05:04:09 +1100

    add comments for hop file

--- interpolation.hpp
+++ interpolation.hpp
@@ -9,11 +9,22 @@
 #ifndef interpolation_hpp
 #define interpolation_hpp
 
+
 #include <iostream>
 #include <vector>
 
+
 using namespace std;
 
+
+// This namespace includes five functions to do interpolatio.
+// The first two of them are for 1D interpolation and next two of them are for 2D interpolation.
+// The last one is an algorithm to solve tridiagonal matrices
+// 1. linear2 -> linear interpolation 2D
+// 2. spline2 -> spline interpolation 2D
+// 3. linear1 -> linear interpolation 1D
+// 4. spline1 -> spline interpolation 1D
+// 5. tama    -> Thomas Algorithm
 namespace Interpolation {
     void tdma(vector<double> &M, vector<double> a, vector<double> b, vector<double> c);
     void spline(vector<double> x, vector<double> y, vector<double> xi, vector<double> &yi);
